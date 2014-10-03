97645ea4aa5a2f08cd075aa88a72c51a371fcb45
Chen Huang <alan.tolearn@gmail.com>
2014-10-03 15:25:00 -0400

    Add create method to url controller and link index to new

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -6,4 +6,16 @@ class UrlsController < ApplicationController
   def new
     @url = Url.new
   end
+
+  def create
+    Url.create(url_params)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original)
+  end
 end
